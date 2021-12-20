3360ef9f7751bc5ed6ec09324348411d9f7518b2
TiiLeaf <sam.jansen@yahoo.com>
2021-12-20 14:36:52 -0700

    add more rendering methods

--- Mandelbrot/Form1.Designer.cs
+++ Mandelbrot/Form1.Designer.cs
@@ -40,24 +40,27 @@
             this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.statusStripTimeLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.renderTimeLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.bitmapStitchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multithreaded16ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.multithreadedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.singleThreadedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.multithreaded8CanvasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.singlethreadedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bitmapStitching16ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
             this.toolStrip1.SuspendLayout();
-            this.statusStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // canvas
             // 
             this.canvas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.canvas.Location = new System.Drawing.Point(0, 24);
+            this.canvas.Location = new System.Drawing.Point(0, 27);
             this.canvas.Margin = new System.Windows.Forms.Padding(0);
             this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(512, 521);
+            this.canvas.Size = new System.Drawing.Size(512, 512);
             this.canvas.TabIndex = 0;
             this.canvas.TabStop = false;
             // 
@@ -160,60 +163,83 @@
             this.toolStripButton6.Text = "v";
             this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
             // 
-            // statusStrip1
+            // toolStrip2
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripDropDownButton1,
-            this.statusStripTimeLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 544);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(512, 22);
-            this.statusStrip1.TabIndex = 2;
-            this.statusStrip1.Text = "statusStrip1";
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renderTimeLabel,
+            this.toolStripSeparator2,
+            this.toolStripDropDownButton1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 541);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(512, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
             // 
-            // statusStripTimeLabel
+            // renderTimeLabel
             // 
-            this.statusStripTimeLabel.Name = "statusStripTimeLabel";
-            this.statusStripTimeLabel.Size = new System.Drawing.Size(101, 17);
-            this.statusStripTimeLabel.Text = "Render Time: 0ms";
+            this.renderTimeLabel.Name = "renderTimeLabel";
+            this.renderTimeLabel.Size = new System.Drawing.Size(101, 22);
+            this.renderTimeLabel.Text = "Render Time: 0ms";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
             // 
             // toolStripDropDownButton1
             // 
             this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.multithreaded8CanvasToolStripMenuItem,
-            this.singleThreadedToolStripMenuItem,
-            this.multithreadedToolStripMenuItem});
+            this.bitmapStitching16ToolStripMenuItem,
+            this.bitmapStitchingToolStripMenuItem,
+            this.multithreaded16ToolStripMenuItem,
+            this.multithreadedToolStripMenuItem,
+            this.singlethreadedToolStripMenuItem});
             this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
             this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(119, 20);
-            this.toolStripDropDownButton1.Text = "Rendering Method";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(104, 22);
+            this.toolStripDropDownButton1.Text = "Single-threaded";
+            this.toolStripDropDownButton1.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripDropDownButton1_DropDownItemClicked);
+            // 
+            // bitmapStitchingToolStripMenuItem
+            // 
+            this.bitmapStitchingToolStripMenuItem.Name = "bitmapStitchingToolStripMenuItem";
+            this.bitmapStitchingToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.bitmapStitchingToolStripMenuItem.Text = "Bitmap Stitching";
+            // 
+            // multithreaded16ToolStripMenuItem
+            // 
+            this.multithreaded16ToolStripMenuItem.Name = "multithreaded16ToolStripMenuItem";
+            this.multithreaded16ToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.multithreaded16ToolStripMenuItem.Text = "Multi-threaded (16)";
             // 
             // multithreadedToolStripMenuItem
             // 
             this.multithreadedToolStripMenuItem.Name = "multithreadedToolStripMenuItem";
-            this.multithreadedToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
-            this.multithreadedToolStripMenuItem.Text = "Multithreaded";
+            this.multithreadedToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.multithreadedToolStripMenuItem.Text = "Multi-threaded";
             // 
-            // singleThreadedToolStripMenuItem
+            // singlethreadedToolStripMenuItem
             // 
-            this.singleThreadedToolStripMenuItem.Name = "singleThreadedToolStripMenuItem";
-            this.singleThreadedToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
-            this.singleThreadedToolStripMenuItem.Text = "Single Threaded";
+            this.singlethreadedToolStripMenuItem.Name = "singlethreadedToolStripMenuItem";
+            this.singlethreadedToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.singlethreadedToolStripMenuItem.Text = "Single-threaded";
             // 
-            // multithreaded8CanvasToolStripMenuItem
+            // bitmapStitching16ToolStripMenuItem
             // 
-            this.multithreaded8CanvasToolStripMenuItem.Name = "multithreaded8CanvasToolStripMenuItem";
-            this.multithreaded8CanvasToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
-            this.multithreaded8CanvasToolStripMenuItem.Text = "Multithreaded 8 Canvas";
+            this.bitmapStitching16ToolStripMenuItem.Name = "bitmapStitching16ToolStripMenuItem";
+            this.bitmapStitching16ToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.bitmapStitching16ToolStripMenuItem.Text = "Bitmap Stitching (16)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(512, 566);
-            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip2);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.canvas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -225,8 +251,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,11 +271,14 @@
         private ToolStripSeparator toolStripSeparator1;
         private ToolStripLabel toolStripLabel1;
         private ToolStripLabel toolStripLabel2;
-        private StatusStrip statusStrip1;
-        private ToolStripStatusLabel statusStripTimeLabel;
+        private ToolStrip toolStrip2;
+        private ToolStripLabel renderTimeLabel;
+        private ToolStripSeparator toolStripSeparator2;
         private ToolStripDropDownButton toolStripDropDownButton1;
-        private ToolStripMenuItem multithreaded8CanvasToolStripMenuItem;
-        private ToolStripMenuItem singleThreadedToolStripMenuItem;
+        private ToolStripMenuItem bitmapStitchingToolStripMenuItem;
         private ToolStripMenuItem multithreadedToolStripMenuItem;
+        private ToolStripMenuItem singlethreadedToolStripMenuItem;
+        private ToolStripMenuItem multithreaded16ToolStripMenuItem;
+        private ToolStripMenuItem bitmapStitching16ToolStripMenuItem;
     }
 }
--- Mandelbrot/Form1.cs
+++ Mandelbrot/Form1.cs
@@ -9,7 +9,7 @@ namespace Mandelbrot {
         double[] topLeftCoord = { -2.0, 1.5 };
         double[] bottomRightCoord = { 1.0, -1.5 };
         double zoomScale = 1.0;
-        double[] offset = {0.0, 0.0};
+        double[] offset = { 0.0, 0.0 };
 
         public Camera(int _screenWidth, int _screenHeight) {
             screenHeight = _screenHeight;
@@ -17,7 +17,7 @@ namespace Mandelbrot {
         }
 
         public void zoom(double zoomChange) {
-            zoomScale += zoomScale * zoomChange;
+            zoomScale += zoomScale * zoomChange * 0.3;
             topLeftCoord[0] = -2.0 / zoomScale;
             topLeftCoord[1] = 1.5 / zoomScale;
             bottomRightCoord[0] = 1.0 / zoomScale;
@@ -32,21 +32,23 @@ namespace Mandelbrot {
         public void calculateAxisScales() {
             xScale = (bottomRightCoord[0] - topLeftCoord[0]) / (double)screenWidth;
             yScale = (bottomRightCoord[1] - topLeftCoord[1]) / (double)screenHeight;
- 
+
         }
 
         public double[] screenToCameraCoords(int x, int y) {
-            double[] result = { x * xScale + topLeftCoord[0] + offset[0], y * yScale + topLeftCoord[1] + offset[1]};
+            double[] result = { x * xScale + topLeftCoord[0] + offset[0], y * yScale + topLeftCoord[1] + offset[1] };
             return result;
         }
     }
 
     public partial class Form1 : Form {
+        static int maxIterations = 100;
+        static string renderMethod = "Single-threaded";
 
-        public static Camera camera;
-        public static int maxIterations = 100;
-        public static Color[] palette = new Color[maxIterations];
-        public int[,] iteratonCounts = new int[0,0];
+        int[,] iteratonCounts = new int[0,0];
+        Bitmap[] bitmaps = new Bitmap[16];
+        static Camera camera;
+        static Color[] palette = new Color[maxIterations];
 
         public Form1() {
             InitializeComponent();
@@ -60,45 +62,109 @@ namespace Mandelbrot {
             }
             palette[maxIterations - 1] = Color.Black;
             drawMandelbrot();
-            statusStrip1.SizingGrip = false;
         }
 
         private void drawMandelbrot() {
             DateTime startTime = DateTime.Now;
-            if (false) {
-                camera.calculateAxisScales();
-
-                Thread[] threads = new Thread[8];
-                for (int i = 0; i < threads.Length; i++) {
-                    threads[i] = new Thread(() => {
-                        calculateMandelbrotStrip(0, canvas.Height);
-                    });
-                    threads[i].Name = "Worker #" + i;
-                    threads[i].Start();
-                }
-
-                for (int i = 0; i < threads.Length; i++) {
-                    threads[i].Join();
-                }
-
-                Bitmap bmp = new Bitmap(canvas.Width, canvas.Height);
-                for (int y = 0; y < canvas.Height; y++) {
-                    for (int x = 0; x < canvas.Width; x++) {
-                        bmp.SetPixel(x, y, palette[iteratonCounts[x, y]]);
-                    }
-                }
-                canvas.Image = bmp;
-            } else { 
-                drawMandelbrotSingleThread();
+            switch (renderMethod) {
+                case "Multi-threaded":
+                    drawMandelbrotMultithreaded(8);
+                    break;
+                case "Multi-threaded (16)":
+                    drawMandelbrotMultithreaded(16);
+                    break;
+                case "Bitmap Stitching":
+                    drawMandelbrotMultipleBitmaps(8);
+                    break;
+                case "Bitmap Stitching (16)":
+                    drawMandelbrotMultipleBitmaps(16);
+                    break;
+                default:
+                    drawMandelbrotSingleThread();
+                    break;
             }
 
-            DateTime endTime = DateTime.Now;
-            TimeSpan elapsed = (endTime - startTime);
-            statusStripTimeLabel.Text = "Render Time: " + Math.Floor(elapsed.TotalMilliseconds) + "ms";
+            TimeSpan elapsed = (DateTime.Now - startTime);
+            renderTimeLabel.Text = "Render Time: " + Math.Floor(elapsed.TotalMilliseconds) + "ms";
         }
 
-        public void calculateMandelbrotStrip(int startY, int endY) {
+        private void drawMandelbrotMultipleBitmaps(int threadCount) {
+            camera.calculateAxisScales();
+            Thread[] threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++) {
+                bitmaps[i] = new Bitmap(canvas.Width, canvas.Height);
+            }
+
+            for (int i = 0; i < threadCount; i++) {
+                int startY = (canvas.Height / threadCount) * i;
+                int endY = (canvas.Height / threadCount) * (i + 1);
+                int index = i;
+                threads[i] = new Thread(() => {
+                    drawMandelbrotStrip(startY, endY, index);
+                });
+                threads[i].Name = "Worker #" + i;
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threadCount; i++) {
+                threads[i].Join();
+            }
+
+            Graphics g = canvas.CreateGraphics();
+            for (int i = 0; i < threadCount; i++) {
+                g.DrawImage(bitmaps[i], new Point(0, 0));
+            }
+        }
+
+        private void drawMandelbrotStrip(int startY, int endY, int index) {
+            for (int y = startY; y < endY; y++) {
+                for (int x = 0; x < canvas.Width; x++) {
+                    double[] coords = camera.screenToCameraCoords(x, y);
+                    Complex c = new Complex(coords[0], coords[1]); //position in complex plane
+                    Complex z = new Complex(0, 0); //the number to be iterated
+
+                    int i = 0;
+                    while (i < maxIterations && z.magSquared() < 4.0) {
+                        i++;
+                        z.square();
+                        z.add(c);
+                    }
+
+                    bitmaps[index].SetPixel(x, y, palette[i - 1]);
+                }
+            }
+        }
+
+        private void drawMandelbrotMultithreaded(int threadCount) {
+            camera.calculateAxisScales();
+
+            Thread[] threads = new Thread[threadCount];
+            for (int i = 0; i < threadCount; i++) {
+                int startY = (canvas.Height / threadCount) * i;
+                int endY = (canvas.Height / threadCount) * (i + 1);
+                threads[i] = new Thread(() => {
+                    calculateMandelbrotStrip(startY, endY);
+                });
+                threads[i].Name = "Worker #" + i;
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threadCount; i++) {
+                threads[i].Join();
+            }
+
+            Bitmap bmp = new Bitmap(canvas.Width, canvas.Height);
             for (int y = 0; y < canvas.Height; y++) {
+                for (int x = 0; x < canvas.Width; x++) {
+                    bmp.SetPixel(x, y, palette[iteratonCounts[x, y]]);
+                }
+            }
+            canvas.Image = bmp;
+        }
+
+        private void calculateMandelbrotStrip(int startY, int endY) {
+            for (int y = startY; y < endY; y++) {
                 for (int x = 0; x < canvas.Width; x++) {
                     double[] coords = camera.screenToCameraCoords(x, y);
                     Complex c = new Complex(coords[0], coords[1]); //position in complex plane
@@ -140,13 +206,14 @@ namespace Mandelbrot {
             canvas.Image = bmp;
         }
 
+        #region UI_EVENTS
         private void toolStripButton1_Click(object sender, EventArgs e) {
-            camera.zoom(0.25);
+            camera.zoom(1);
             drawMandelbrot();
         }
 
         private void toolStripButton2_Click(object sender, EventArgs e) {
-            camera.zoom(-0.25);
+            camera.zoom(-1);
             drawMandelbrot();
         }
 
@@ -169,6 +236,13 @@ namespace Mandelbrot {
            camera.pan(0.0, -0.25);
            drawMandelbrot();
         }
+
+        private void toolStripDropDownButton1_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e) {
+            renderMethod = e.ClickedItem.Text;
+            toolStripDropDownButton1.Text = e.ClickedItem.Text;
+            drawMandelbrot();
+        }
+        #endregion
     }
 }
 
